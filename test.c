0194ea133449e0c366e734f9d4cf9c62c9660284
bigdiggysquiggle <dylanromansky@yahoo.com>
2022-07-09 01:15:40 -0700

    yw:

--- test.c
+++ test.c
@@ -3,6 +3,7 @@
 
 int	ft_strlen(char const *s);
 char *ft_strcpy(char *restrict dst, const char *src);
+int ft_strcmp(char const *s1, char const *s2);
 
 int main(void)
 {
@@ -13,4 +14,5 @@ int main(void)
 	printf("%s\n", beans);
 	ft_strcpy(beans, "peans");
 	printf("%s\n", beans);
+	printf("%d\n", ft_strcmp("beans", "benas"));
 }
